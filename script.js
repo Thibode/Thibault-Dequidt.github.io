de3e1151b317cef381ae67e8634a7464c01edb11
Thibode <th.dequidt@gmail.com>
2021-09-15 17:32:06 +0200

    Ajout de Typed et modification au niveau html et css pour la class container

---
+++ script.js
@@ -0,0 +1,8 @@
+// Typed
+var typed = new Typed(".typed", {
+  strings: [
+    "Bonjour à tous,",
+    "Après de multiples expérience dans différents milieu professionnel (vous pourrez le constater dans mon CV ci-dessous). J'ai décidé de me lancer dans l'apprentissage du code. J'ai commencé par voir les bases de la création de site web en apprennant HTML et CSS. Depuis peu je me met à étudier Javascript et ses différents Frameworks. Aujourd'hui je souhaite réaliser une formation pour obtenir un dîplome et je suis à la recherche d'une entreprise pouvant m'accueillir pour un contrat d'apprentissage ou un stage si je trouve le moyen de faire un financement personnel.",
+  ],
+  typeSpeed: 40,
+});
