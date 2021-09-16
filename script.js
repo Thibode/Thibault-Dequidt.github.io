d7e46c640f3d4fb8b77174a5e83ae90ac4530807
Thibode <th.dequidt@gmail.com>
2021-09-16 16:00:56 +0200

    Partie Formations CSS ok, ajout de la partie parcours

--- script.js
+++ script.js
@@ -18,7 +18,10 @@ window.addEventListener("scroll", function () {
 var typed = new Typed(".typed", {
   strings: [
     "Bonjour à tous,",
-    "Après de multiples expériences dans différents milieu professionnel <br>(Vous pourrez le constater dans mon CV ci-dessous).<br>J'ai décidé de me lancer dans l'apprentissage du code. J'ai commencé par voir les bases de la création de site web en apprennant HTML et CSS. Depuis peu je me met à étudier le langage Javascript et ses différents Frameworks. Aujourd'hui je souhaite réaliser une formation pour obtenir un dîplome et je suis à la recherche d'une entreprise pouvant m'accueillir pour un contrat d'apprentissage ou un stage si je trouve le moyen de faire un financement personnel.",
+    "Après de multiples expériences dans différents milieu professionnel <br>(Vous pourrez le constater dans mon CV ci-dessous).<br>J'ai décidé de me lancer dans l'apprentissage du code. J'ai commencé par voir les bases de la création de site web en apprennant HTML et CSS. Depuis peu je me met à étudier le langage Javascript et ses différents Frameworks. Aujourd'hui je souhaite réaliser une formation pour avoir plus de compétences et au pour l'obtention d'un diplôme. <br>Je suis donc à la recherche d'une entreprise pouvant m'accueillir pour un contrat d'apprentissage ou un stage si je trouve le moyen de faire un financement personnel.",
   ],
   typeSpeed: 40,
 });
+
+//AOS
+AOS.init();
